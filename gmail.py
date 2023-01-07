b2e4ef7fba0c2429ca269b1f1763d74e4992ae87
jandginvestment <jandginvestment@gmail.com>
2023-01-07 16:14:45 +0400

    codecs related changes

--- gmail.py
+++ gmail.py
@@ -3,7 +3,6 @@
 from os import name
 import smtplib
 import csv
-import codecs
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email import encoders
@@ -16,7 +15,7 @@ from email.mime.application import MIMEApplication
 #The mail addresses and password
 sender_address = 'jandginvestment@gmail.com'
 # https://myaccount.google.com/u/1/security
-sender_pass = 'pftcyijwtqvosrkk'
+sender_pass = 'vdkjdevbsgmoinpv'
 #Create SMTP session for sending the mail
 session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
 session.starttls() #enable security
@@ -32,7 +31,7 @@ with open('2addresses.csv','r') as file:
         message['Subject'] = 'Seeking .NET/ Angular full stack consultant openings in Dubai/ Sharjah/ Ajman/Abhudhabi'   #The subject line
         #The body and the attachments for the mail
         # read mail content from a html file
-        f=codecs.open("body2.html", 'r', 'utf-8').read()
+        f = open("body1.html", 'r', encoding='utf-8').read()
         mail_content =str( f).replace('{{Name}}',receiver_address.split('@')[0].split('.')[0].capitalize())
         message.attach(MIMEText(mail_content, 'html'))
         filename = "jandg.docx"
